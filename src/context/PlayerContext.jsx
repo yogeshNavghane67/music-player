70d2e0ebbc95a0baafefdd5d7ccad4b891d56a55
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-28 14:57:46 +0530

    previous, next & seekSong

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -32,9 +32,29 @@ const PlayerContextProvider = (props) => {
   };
 
   const playWithId = async (id) => {
- await setTrack(songsData[id]);
- await audioRef.current.play();
- setPlayStatus(true);
+    await setTrack(songsData[id]);
+    await audioRef.current.play();
+    setPlayStatus(true);
+  };
+
+  const previous = async () => {
+    if(track.id>0) {
+        await setTrack(songsData[track.id-1]);
+        await audioRef.current.play()
+        setPlayStatus(true);
+    }
+  }
+
+  const next = async () => {
+    if(track.id < songsData.length-1) {
+        await setTrack(songsData[track.id+1]);
+        await audioRef.current.play()
+        setPlayStatus(true);
+    }
+  }
+
+  const seekSong = async (e) => {
+    audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.offsetWidth)*audioRef.current.duration)
   }
 
   useEffect(() => {
@@ -71,6 +91,8 @@ const PlayerContextProvider = (props) => {
     play,
     pause,
     playWithId,
+    previous,next,
+    seekSong
   };
 
   return (
